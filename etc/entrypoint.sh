8aab178f2bd861fad3a24c1b80ec0b213a3d1033
Seth van Ommen <seth.vanommen@billups.com>
2019-04-01 17:35:00 -0700

    entrypoint spec fix

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 confd -onetime -backend ssm --prefix /${ENVIRONMENT} -node https://ssm.us-west-2.amazonaws.com
-exec ["/opt/tegola", "--config", "/opt/config.toml", "serve"]
+exec /opt/tegola --config /opt/config.toml serve
